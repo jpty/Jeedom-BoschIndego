effafbc4c02b58d031233d04fde514521ff60b3e
jpty <1798636+jpty@users.noreply.github.com>
2019-05-07 19:33:12 +0200

    Add files via upload

---
+++ indego_auth.php
@@ -0,0 +1,5 @@
+<?php
+	include_once('indego.php');
+  	$wilson = new indego();
+	$wilson->authenticate();
+?>
---
+++ indego_doAction.php
@@ -0,0 +1,6 @@
+<?php
+	include('indego.php');
+	$wilson = new indego();
+	$wilson->doAction($_GET['cmd']);
+
+?>
